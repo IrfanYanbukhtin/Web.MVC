cacafa11f2680c224f562ec74897f823f00a9208
user <user@WIN-JV83T09S6R3>
2023-05-11 13:16:11 +0400

    Добавьте файлы проекта.

---
+++ Web.MVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models;
+
+namespace Web.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var students = new List<Student>
+            {
+                new Student
+                {
+                    Id= 1,
+                    Firstname = "Irfan",
+                    Lastname = "Yanbuxtin",
+                    Age = 24,
+                },
+
+                new Student
+                {
+                    Id= 2,
+                    Firstname = "Sahlar",
+                    Lastname = "Haciyev",
+                    Age = 26,
+                },
+
+                new Student
+                {
+                    Id= 3,
+                    Firstname = "Elvin",
+                    Lastname = "Cebrailov",
+                    Age = 24,
+                },
+            };
+
+            return View(students);
+        }
+    }
+}
---
+++ Web.MVC/Controllers/StudentController1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models;
+
+namespace Web.MVC.Controllers
+{
+    public class StudentController1 : Controller
+    {
+        private List<Student> _students = new List<Student>
+            {
+               new Student
+                {
+                    Id= 1,
+                    Firstname = "Irfan",
+                    Lastname = "Yanbuxtin",
+                    Age = 24,
+                },
+
+                new Student
+                {
+                    Id= 2,
+                    Firstname = "Sahlar",
+                    Lastname = "Haciyev",
+                    Age = 26,
+                },
+
+                new Student
+                {
+                    Id= 3,
+                    Firstname = "Elvin",
+                    Lastname = "Cebrailov",
+                    Age = 24,
+                },
+            };
+
+
+        public IActionResult Index()
+        {
+            return View(_students);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = _students.FirstOrDefault(x => x.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+    }
+}
